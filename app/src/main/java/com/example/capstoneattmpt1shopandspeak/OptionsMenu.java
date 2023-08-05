b5146ec2b8aab480c50c42064f84ba01eea4f9a7
Angelo leDonne <ledonneangel02@gmail.com>
2023-08-05 15:30:26 -0700

    Created Options Menu

---
+++ app/src/main/java/com/example/capstoneattmpt1shopandspeak/OptionsMenu.java
@@ -0,0 +1,14 @@
+package com.example.capstoneattmpt1shopandspeak;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OptionsMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_options_menu);
+    }
+}
