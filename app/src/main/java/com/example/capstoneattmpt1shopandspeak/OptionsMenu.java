a8e4b05afebc60bc73fe78a1711fb2c15fd7d8b1
Angelo leDonne <ledonneangel02@gmail.com>
2023-08-14 11:01:23 -0500

    Made sure the buttons are black on white

--- app/src/main/java/com/example/capstoneattmpt1shopandspeak/OptionsMenu.java
+++ app/src/main/java/com/example/capstoneattmpt1shopandspeak/OptionsMenu.java
@@ -20,24 +20,35 @@ public class OptionsMenu extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_options_menu);
 
+        Button RTM = findViewById(R.id.ReturnHomeBtn);
+        RTM.setOnClickListener(v -> ReturnHome());
+        Button SpeakCommand = findViewById(R.id.SpeakACommand);
+        SpeakCommand.setOnClickListener(v -> OpenSpeakACommand());
+
         //ColorScheme for different visual impairments
         List<String> ColorScheme = new ArrayList<String>();
         ColorScheme.add("Black on White");
         ColorScheme.add("High Contrast");
         ColorScheme.add("White on Black");
 
+        //Spinner Adapter Boilerplate Code
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(
-          this, android.R.layout.simple_spinner_item, ColorScheme);
-
+                this, android.R.layout.simple_spinner_item, ColorScheme);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         Spinner spinner = (Spinner) findViewById(R.id.ColorSchemeSpinner);
         spinner.setAdapter(adapter);
 
-        Button RTM = findViewById(R.id.ReturnHomeBtn);
-        RTM.setOnClickListener(v -> ReturnHome());
+        //Selected Theme string, so we can save the activity
+        String selectedTheme = spinner.getSelectedItem().toString();
     }
+
     private void ReturnHome(){
         Intent ReturnHome = new Intent(OptionsMenu.this, MainActivity.class);
         startActivity(ReturnHome);
+    }
+
+    private void OpenSpeakACommand(){
+        Intent SpeakCommands = new Intent(OptionsMenu.this, SpeechText.class);
+        startActivity(SpeakCommands);
     }
 }
